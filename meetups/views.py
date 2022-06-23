b75d642b9d95fc117626b96e68271f3bcc517c95
arisafello <felomwansa@mail.com>
2022-06-23 15:28:45 +0300

    Addition of html beautification

--- meetups/views.py
+++ meetups/views.py
@@ -2,4 +2,36 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {
+            'title': 'A First Meetup',
+            'Location': 'Naivasha',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'Location': 'Nairobi',
+            'slug': 'a-Second-meetup'
+        },
+        {
+            'title': 'A Third Meetup',
+            'Location': 'Mombasa',
+            'slug': 'a-Third-meetup'
+        },
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
+
+
+def meetup_details(request):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup!'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+
+    })
