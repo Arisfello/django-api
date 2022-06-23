2e95fa07124f6efda1842529676fccd48fbe223e
arisafello <felomwansa@mail.com>
2022-06-23 17:17:13 +0300

    Addition of meetup-details.html

--- meetups/views.py
+++ meetups/views.py
@@ -25,7 +25,7 @@ def index(request):
     })
 
 
-def meetup_details(request):
+def meetup_details(request, meetup_slug):
     selected_meetup = {
         'title': 'A First Meetup',
         'description': 'This is the first meetup!'
