0a7bb17c1c9bd2133aa4d4152bfb0184a2a217cd
arisafello <felomwansa@mail.com>
2022-07-01 13:39:10 +0300

    adding the organizer email field and date field

--- meetups/models.py
+++ meetups/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+# Create your models here.
+
 class Location(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=300)
@@ -18,6 +20,8 @@ class Participant(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
